1bb8aa4b18950b6c37d4ac9c18537a4a8465eea6
XanderCodez <ahmed.ayman.wehda.co@gmail.com>
2024-08-29 18:13:19 +0300

    Add files via upload

--- Scripts/Display Name.js
+++ Scripts/Display Name.js
@@ -1,9 +1,31 @@
 document.getElementById("submit").onclick = function () {
    let coolName = document.getElementById("nameInput").value.trim();
-   if (coolName) {
-      document.getElementById("textHere").textContent = `welcome, ${coolName}!`;
-      document.getElementById("msg").textContent = "";
+   if (coolName && !/^[0-9]+$/.test(coolName)) {
+      document.getElementById("textHere").textContent = ` ${coolName}`;
+      document.getElementById("msg").textContent = "Welcome!";
       document.getElementById("error").textContent = "";
+
+      let size = 50;
+      document.getElementById("increase").onclick = function () {
+         size += 5;
+         document.getElementById("textHere").style.fontSize = `${size}px`;
+      };
+      document.getElementById("decrease").onclick = function () {
+         size -= 5;
+         document.getElementById("textHere").style.fontSize = `${size}px`;
+      };
+      document.getElementById("reset").onclick = function () {
+         size = 50;
+         document.getElementById("textHere").style.fontSize = `${size}px`;
+      };
+      document.getElementById("clear").onclick = function () {
+         document.getElementById("textHere").textContent = "";
+         document.getElementById("msg").textContent = "Type another name";
+      };
+   } else if (/^[0-9]+$/.test(coolName)) {
+      document.getElementById("textHere").textContent = "";
+      document.getElementById("msg").textContent = "";
+      document.getElementById("error").textContent = "Names can't be numbers!";
    } else {
       document.getElementById("error").textContent = "Type your name for magic!";
       document.getElementById("textHere").textContent = "";
