99c4639f12351c58dbe0d030324c0f3d4c52bf11
XanderCodez <ahmed.ayman.wehda.co@gmail.com>
2024-09-03 19:35:08 +0300

    Add files via upload
    New bug fixes

--- Scripts/Number Guess.js
+++ Scripts/Number Guess.js
@@ -1,16 +1,21 @@
-// need to make sure that it exists when Max is lower than Min
-
 document.getElementById("start").onclick = function () {
-   let minNum = window.prompt("Add min number");
+   let minNum = window.prompt("Add minimum value").trim();
    let running;
    let tries = 0;
    let answer;
 
-   if (minNum !== null && minNum !== "") {
-      let maxNum = prompt("add max number");
+   if (minNum !== null && minNum !== "" && !isNaN(minNum)) {
+      let maxNum = prompt("Add maximum value").trim();
       if (maxNum !== null && maxNum !== "") {
-         running = true;
-         answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+         maxNum = Number(maxNum);
+         minNum = Number(minNum);
+         if (minNum >= maxNum) {
+            alert("And higher value to the Maximum and try again");
+            running = false;
+         } else {
+            answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+            running = true;
+         }
       }
       while (running) {
          let guess = window.prompt(`Geuss a number between ${minNum} - ${maxNum}`);
@@ -26,14 +31,19 @@ document.getElementById("start").onclick = function () {
             } else if (guess > answer) {
                alert("a bit lower!");
             } else {
-               alert(`Correct!, the answer is ${answer}, it took you ${tries} times`);
                running = false;
+               if (tries === 1) {
+                  alert(`Wow, you guessed it correctly on the first try!`);
+               } else if (5 >= tries) {
+                  alert(`${answer} is correct!, it took you only ${tries} tries, thats fast!`);
+               } else {
+                  alert(`${answer} is the Number, it took you ${tries} times`);
+               }
             }
          }
       }
    } else {
+      alert("Game canceld, try again!");
       running = false;
    }
 };
-//  need to exist the the whole game if
-//  press Ok on an empty value in Min and then Max Or pressed Cancel
